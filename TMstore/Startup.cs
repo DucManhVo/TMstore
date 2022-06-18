97875244b5e205c64e9021ca125a8f6ad79f89f1
Duc Manh <voducmanh000@gmail.com>
2022-06-18 14:09:22 +0700

    Add project files.

---
+++ TMstore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TMstore.Startup))]
+namespace TMstore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
